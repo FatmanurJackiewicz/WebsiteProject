1c798169cebf2b3b3d1f5b72aff27728b6ccc5e0
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-14 09:48:54 +0100

    FileUğloadModel created

--- FileAPI/ViewModel/FileUploadModel.cs
+++ FileAPI/ViewModel/FileUploadModel.cs
@@ -1,6 +1,12 @@
-﻿namespace FileAPI.ViewModel
+﻿namespace FileAPI.ViewModel;
+
+public class FileUploadModel
 {
-    public class FileUploadModel
-    {
-    }
+    public IFormFile File { get; set; }
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Author { get; set; }
+
+
 }

f03eca35de126854525285580fa3d8e032015b60
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-14 10:33:45 +0100

    Single Folder Upload Action created

--- FileAPI/Controllers/FileController.cs
+++ FileAPI/Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FileAPI.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FileAPI.Controllers
@@ -7,5 +8,42 @@ namespace FileAPI.Controllers
     [ApiController]
     public class FileController : ControllerBase
     {
+
+        [HttpPost("upload"), DisableRequestSizeLimit]
+        public async Task<IActionResult> UploadFile([FromForm] FileUploadModel model)
+        {
+            if(model.File==null && model.File.Length == 0)
+            {
+                return BadRequest("Invalid File");
+            }
+
+            var folderName = Path.Combine("Resources", "AllFiles");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+            if (!Directory.Exists(pathToSave))
+            {
+                Directory.CreateDirectory(pathToSave);
+            }
+            var fileName = model.File.FileName;
+            //c://resources/repos/allfiles/fileName.jpg
+            var fullPath = Path.Combine(pathToSave, fileName);
+            var dbPath = Path.Combine(folderName, fileName);
+
+            if (System.IO.File.Exists(fullPath))
+            {
+                return BadRequest("File already exists!");
+            }
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                model.File.CopyTo(stream);
+            }
+
+            return Ok(new { dbPath });
+        }
+
+
+
+
+
     }
 }

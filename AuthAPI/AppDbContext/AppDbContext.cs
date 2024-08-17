870631c09d56f156817e472c914b0b2da94d26cd
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-17 16:03:28 +0100

    AppDbContext created

---
+++ AuthAPI/AppDbContext/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AuthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace AuthAPI.AppDbContext;
+
+public partial class AppDbContext:DbContext
+{
+
+    public AppDbContext (DbContextOptions <AppDbContext> options)
+        :base(options)
+    {
+
+    }
+
+    public virtual DbSet<UserModel> UserModel { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserModel>(entity =>
+        {
+            entity.HasKey(k => k.Id);
+        });
+        OnModelCreatingPartial(modelBuilder);
+    }
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+
+}

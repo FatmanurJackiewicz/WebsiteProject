39ef7188647f1c974b6b61ce1981b43bf133bae4
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-17 15:13:02 +0100

    UserModel created

---
+++ AuthAPI/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthAPI.Models;
+
+public class UserModel
+{
+    [PrimaryKey]
+    public int Id { get; set; }
+
+    [MaxLength(50)]
+    public string Username { get; set; }
+
+    [MaxLength(255)]
+    public required IPasswordHasher<PasswordHasher> PasswordHash;
+
+    
+    public IdentityRole Role { get; set; }
+
+
+}

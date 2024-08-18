5a4135d1b5364c67ce5868914545cd9216592b8d
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-18 18:15:21 +0100

    RegisterDtoValidator created

--- AuthAPI/Dto/RegisterDto.cs
+++ AuthAPI/Dto/RegisterDto.cs
@@ -4,17 +4,7 @@ namespace AuthAPI.Dto
 {
     public class RegisterDto
     {
-        [Required]
         public string Username { get; set; }
-
-        [Required]
-        public EmailAddressAttribute Email { get; set; }
-
-        [Required, DataType(DataType.Password)]
         public string Password { get; set; }
-
-        [Required,Compare(nameof(Password)), DataType(DataType.Password)]
-        public string ConfirmPassword { get; set; }
-
     }
 }
---
+++ AuthAPI/Dto/RegisterDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace AuthAPI.Dto;
+
+public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+{
+    public RegisterDtoValidator()
+    {
+        RuleFor(x => x.Username).NotEmpty().WithMessage("Username can not be empty!");
+        RuleFor(x => x.Password).NotEmpty().WithMessage("Password can not be empty!");
+        RuleFor(x => x.Password).MinimumLength(8).WithMessage("Password should be minimum 8 character!");
+    }
+
+
+
+
+
+}

8b0657ac4eaae4e28d84c30c5a0159dcb1310242
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-20 16:09:17 +0100

    Login action created

--- AuthAPI/Controllers/AuthController.cs
+++ AuthAPI/Controllers/AuthController.cs
@@ -11,6 +11,24 @@ namespace AuthAPI.Controllers
     {
         private static readonly List<User> _users = new();
 
+        [HttpPost]
+        public IActionResult Login([FromForm] LoginDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Ok();
+            }
+            var user = _users.Find(u => u.Email == dto.Email && u.Password == login.Password);
+
+            if (user == null)
+            {
+                return BadRequest("Username or Password is wrong.");
+            }
+
+
+
+        }
+
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromForm] RegisterDto registerDto)
         {

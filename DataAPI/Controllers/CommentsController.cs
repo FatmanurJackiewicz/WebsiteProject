731233dea583e079fe7dce01f84e09ac5c216009
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 12:30:10 +0100

    CommentDtos created

--- DataAPI/Controllers/CommentsController.cs
+++ DataAPI/Controllers/CommentsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DataAPI.Data;
+using DataAPI.DTOs.BlogPosts;
+using DataAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DataAPI.Controllers
@@ -7,6 +10,88 @@ namespace DataAPI.Controllers
     [ApiController]
     public class CommentsController : ControllerBase
     {
+        private readonly AppDbContext _appDbContext;
+
+        public CommentsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetComments([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var blogPosts = await _appDbContext.BlogPosts.FindAsync(id);
+
+            if (blogPosts is null)
+                return NotFound("Comments not found");
+
+            return Ok(blogPosts);
+        }
+
+
+        [HttpPost("createComment")]
+        public async Task<IActionResult> CreateComments([FromBody] CreateCommentsDto createDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var newBlogPosts = new BlogPosts
+            {
+                Title = createDto.Title,
+                Context = createDto.Context,
+                PublishDate = createDto.PublishDate,
+                AuthorId = createDto.AuthorId
+            };
+
+            _appDbContext.BlogPosts.Add(newBlogPosts);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(newBlogPosts);
+        }
+
+        [HttpPut("updateComment")]
+        public async Task<IActionResult> UpdateComment([FromBody] UpdateCommentsDto updateDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var existComments = await _appDbContext.Comments.FindAsync(updateDto.Id);
+
+            if (existComments is null)
+                return NotFound("Comments not found");
+
+
+            existComments.Title = updateDto.Title;
+            existComments.Context = updateDto.Context;
+            existComments.PublishDate = updateDto.PublishDate;
+            existComments.AuthorId = updateDto.AuthorId;
+
+            _appDbContext.BlogPosts.Update(existComments);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(existComments);
+        }
+
+
+        [HttpDelete("deleteComment/{id}")]
+        public async Task<IActionResult> DeleteComments([FromRoute] int id)
+        {
+            var existComments = await _appDbContext.Comments.FindAsync(id);
+
+            if (existComments == null)
+            {
+                return NoContent();
+            }
+
+            _appDbContext.BlogPosts.Remove(existComments);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
 
     }
 }
---
+++ DataAPI/DTOs/Comments/CommentCreateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DataAPI.DTOs.Comments
+{
+    public class CreateCommentDto
+    {
+        public int Id { get; set; }
+        public string Context { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public byte IsApproved { get; set; }
+        public int BlogPostId { get; set; }
+        public int UserId { get; set; }
+    }
+}

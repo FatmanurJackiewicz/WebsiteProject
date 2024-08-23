fc708623f6f16d82204b0db418e25e423b0e0080
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 11:22:42 +0100

    ContactMessages Model created

---
+++ DataAPI/Models/ContactMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAPI.Models
+{
+    public class ContactMessages
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime SentDate { get; set; }
+        public bool IsRead { get; set; }
+
+        public string Reply { get; set; }
+        public DateTime ReplyDate { get; set; }
+    }
+}

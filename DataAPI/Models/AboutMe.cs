b4a1f1164ed4e87d0cd3acdb228f78d520c4eea9
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 20:43:37 +0100

    Revert "Merge pull request #2 from FatmanurJackiewicz/DataAPI-branch"
    This reverts commit 515bffafb2150cba1ad6cf75fdebb7ea48e931d4, reversing
    changes made to eee162d01f920273cc4f5fcc985c6445a3251b8b.

--- DataAPI/Models/AboutMe.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataAPI.Models;
-
-public class AboutMe
-{
-    public int Id { get; set; }
-    public string Introduction { get; set; }
-    public string ImageUrl1 { get; set; }
-    public string ImageUrl2 { get; set; }
-}

46030addd7c8b04d54abf50ec5ee8ad995652b2f
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-08-24 02:06:43 +0300

    asdq

---
+++ AdminPanelMVC/Controllers/AuthAdminController.cs
@@ -0,0 +1,32 @@
+﻿using AdminPanelMVC.Models.AuthAdmin;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanelMVC.Controllers;
+
+public class AuthAdminController : Controller
+{
+	private readonly IHttpClientFactory _httpClientFactory;
+
+    public AuthAdminController(IHttpClientFactory httpClientFactory)
+    {
+		_httpClientFactory = httpClientFactory;
+    }
+
+    [Route("/register")]
+	[HttpGet]
+	public IActionResult Register()
+	{
+		return View();
+	}
+
+	[Route("/register")]
+	[HttpGet]
+	public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+	{
+		if (!ModelState.IsValid)
+			return BadRequest(ModelState);
+
+
+		return View();
+	}
+}

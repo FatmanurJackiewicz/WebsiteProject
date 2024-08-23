f98133ca8988dfe3345ffa9703df705597a3d0b3
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 11:20:44 +0100

    Experiences Model created

---
+++ DataAPI/Models/Experiences.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAPI.Models
+{
+    public class Experiences
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public string Description { get; set; }
+
+    }
+}

15e7601a9689c1302e72f4f43e7b100ccd28dadc
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 11:37:35 +0100

    PersonalInfodto created

---
+++ DataAPI/DTOs/Projects/CreateProjectsDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAPI.DTOs.Projects;
+
+public class CreateProjectsDto
+{
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public string Description { get; set; } = string.Empty;
+
+    [Required]
+    public string ImageUrl { get; set; } = string.Empty;
+}

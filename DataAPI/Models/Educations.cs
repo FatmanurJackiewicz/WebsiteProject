076d798c4f74b547205bc449609d700dd1e4bbe1
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 11:21:06 +0100

    Educations Model created

---
+++ DataAPI/Models/Educations.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAPI.Models
+{
+    public class Educations
+    {
+        public int Id { get; set; }
+        public string Degree { get; set; }
+        public string School { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+
+
+    }
+}

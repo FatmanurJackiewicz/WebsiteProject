f0f8c93ca6df171ed8b58aeced828a12051bbacd
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 11:35:46 +0100

    Configurations added

--- DataAPI/Data/AppDbContext.cs
+++ DataAPI/Data/AppDbContext.cs
@@ -6,28 +6,25 @@ namespace DataAPI.Data
 {
     public class AppDbContext :DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-   : base(options) { }
-
         public DbSet<AboutMe> AboutMe { get; set; }
         public DbSet<ContactMessages> ContactMessages { get; set; }
         public DbSet<Educations> Educations { get; set; }
         public DbSet<Experiences> Experiences { get; set; }
+        public DbSet<PersonalInfo> PersonalInfo { get; set; }
         public DbSet<Projects> Projects { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new AboutMeConfigurations());
-
             modelBuilder.ApplyConfiguration(new ContactMessagesConfigurations());
-
             modelBuilder.ApplyConfiguration(new EducationsConfigurations());
-
             modelBuilder.ApplyConfiguration(new ExperiencesConfigurations());
-
             modelBuilder.ApplyConfiguration(new PersonalInfoConfigurations());
-
             modelBuilder.ApplyConfiguration(new ProjectsConfigurations());
+
             base.OnModelCreating(modelBuilder);
 
 

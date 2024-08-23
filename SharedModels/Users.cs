b6ba6afe59e170491648702a17de9dfbf508cef5
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 17:22:43 +0100

    Shared model added

---
+++ SharedModels/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedModels
+{
+    internal class User
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; internal set; }
+        public byte[] PasswordHash { get; set; }
+        public string? ResetPasswordToken { get; set; }
+        public int RoleId { get; set; }
+
+        public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = null!;
+        public virtual Role Role { get; set; } = default!;
+    }
+}

ecd4a9fdbb6aa1afd3e1070c3d2a723288611372
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-23 11:25:50 +0100

    For models dbset created

--- DataAPI/Data/AppDbContext.cs
+++ DataAPI/Data/AppDbContext.cs
@@ -1,8 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAPI.Data
 {
     public class AppDbContext :DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+   : base(options) { }
+
+        public DbSet<AboutMe> AboutMe { get; set; }
+        public DbSet<ContactMessages> ContactMessages { get; set; }
+        public DbSet<Educations> Educations { get; set; }
+        public DbSet<Experiences> Experiences { get; set; }
+        public DbSet<Projects> Projects { get; set; }
+
     }
 }

6cc6ecab76968a0f590cc2e2286f04db4ecfabd4
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-27 22:50:26 +0100

    PortfoyMVC controllers created

---
+++ PortfoyMVC/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using DataAPI.DTOs.AboutMe;
+using DataAPI.DTOs.ContactMessages;
+using Microsoft.AspNetCore.Mvc;
+using PortfoyMVC.Models;
+
+namespace PortfoyMVC.Controllers;
+
+public class ContactController : Controller
+{
+	private readonly IHttpClientFactory _httpClientFactory;
+
+	public ContactController(IHttpClientFactory httpClientFactory)
+	{
+		_httpClientFactory = httpClientFactory;
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> SubmitContactForm([FromForm] ContactFormViewModel contactFormViewModel)
+	{
+
+		var contactFormDto = new CreateContactMessageDto
+		{
+			Name = contactFormViewModel.Name,
+			Email = contactFormViewModel.Email,
+			Subject = contactFormViewModel.Subject,
+			Message = contactFormViewModel.Message,
+			SentDate = contactFormViewModel.SentDate,
+			IsRead = contactFormViewModel.IsRead,
+			Reply = contactFormViewModel.Reply,
+			ReplyDate = contactFormViewModel.ReplyDate
+		};
+
+		var client = _httpClientFactory.CreateClient("ApiClientData");
+		var response = await client.PostAsJsonAsync("api/contactmessages/createContactMessages", contactFormDto);
+
+		if (!response.IsSuccessStatusCode)
+		{
+			return RedirectToAction("Index", "Home");
+		}
+
+        TempData["SuccessMessage"] = "Contact messages başarıyla gönderildi.";
+
+        return RedirectToAction("Index", "Home");
+	}
+
+}

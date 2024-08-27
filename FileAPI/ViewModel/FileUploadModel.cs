b1547487060c4e4592d1ae315449ee44142d9972
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-27 22:50:06 +0100

    FileAPI FileUploadModel created

--- FileAPI/ViewModel/FileUploadModel.cs
+++ FileAPI/ViewModel/FileUploadModel.cs
@@ -3,10 +3,4 @@
 public class FileUploadModel
 {
     public IFormFile File { get; set; }
-
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public string Author { get; set; }
-
-
 }

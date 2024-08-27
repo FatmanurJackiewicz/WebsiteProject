e9744e40fb38f9eb08f8f59dbe4a45f753e2f32f
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-27 22:51:22 +0100

    PortfoyMVC ViewComponents created

---
+++ PortfoyMVC/ViewComponents/ContactFormViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfoyMVC.Models;
+
+namespace PortfoyMVC.ViewComponents;
+
+public class ContactFormViewComponent : ViewComponent
+{
+	public IViewComponentResult Invoke()
+	{
+		var model = new ContactFormViewModel(); // Modeli doldur veya boş bırak
+		return View(model);
+	}
+}

86def4a19671c1d845042b8d1fd9d6988f8453f3
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-27 22:50:47 +0100

    PortfoyMVC Models created

---
+++ PortfoyMVC/Models/ContactFormViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortfoyMVC.Models;
+
+[Bind("Name, Email, Subject, Message")]
+public class ContactFormViewModel
+{
+	[Required, MaxLength(50)]
+	public string Name { get; set; } = string.Empty;
+
+	[Required, MaxLength(100)]
+	public string Email { get; set; } = string.Empty;
+
+	[Required, MaxLength(100)]
+	public string Subject { get; set; } = string.Empty;
+
+	[Required]
+	public string Message { get; set; } = string.Empty;
+
+	public DateTime SentDate { get; set; } = DateTime.UtcNow;
+
+	public bool IsRead { get; set; } = false;
+
+	public string Reply { get; set; } = string.Empty;
+
+	public DateTime? ReplyDate { get; set; }
+}

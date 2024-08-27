c5ef69131a69a708839558b55e3e861b5455254d
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-27 03:10:11 +0100

    AdminPanelMVC AboutMe Views created

---
+++ AdminPanelMVC/Models/Profile/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminPanelMVC.Models.Profile
+{
+    public class UserViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required, MaxLength(50), EmailAddress]
+        public string Email { get; set; }
+    }
+}

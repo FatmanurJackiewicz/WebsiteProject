6d4559aa4a70d92eaf4b421441158c50e3d3f6e8
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-27 03:08:20 +0100

    UserIdentityFilter created

---
+++ AdminPanelMVC/Filters/UserIdentityFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AdminPanelMVC.Filters
+{
+    public class UserIdentityFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var claimsIdentity = context.HttpContext.User.Identity as ClaimsIdentity;
+            if (claimsIdentity != null)
+            {
+                var userNameClaim = claimsIdentity.FindFirst(ClaimTypes.Name)?.Value;
+
+                if (!string.IsNullOrEmpty(userNameClaim))
+                {
+                    userNameClaim = CapitalizeFirstLetter(userNameClaim);
+                }
+
+                if (context.Controller is Controller controller)
+                {
+                    controller.ViewBag.UserName = userNameClaim;
+                }
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Action tamamlandıktan sonra yapılacak işlemler
+        }
+
+        public static string CapitalizeFirstLetter(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return input.Substring(0, 1).ToUpper() + input.Substring(1);
+        }
+    }
+}

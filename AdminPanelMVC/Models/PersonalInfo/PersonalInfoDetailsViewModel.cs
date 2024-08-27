6be4660bdc0be18090236e345bdda27dc3d630fc
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-27 03:09:40 +0100

    AdminPanelMVC PersonalInfo Models created

---
+++ AdminPanelMVC/Models/PersonalInfo/PersonalInfoDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminPanelMVC.Models.PersonalInfo;
+
+public class PersonalInfoDetailsViewModel
+{
+    public string About { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime BirthDate { get; set; }
+}

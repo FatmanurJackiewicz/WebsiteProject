25553137ad8dbb0f8d790cce5fc799594163d8e5
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-30 00:11:17 +0300

    AdminPanelMVC Experiences Models Created

---
+++ AdminPanelMVC/Models/Experiences/ExperiencesViewModel.cs
@@ -0,0 +1,24 @@
+﻿using AuthAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminPanelMVC.Models.Experiences
+{
+    public class ExperiencesViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Company { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}

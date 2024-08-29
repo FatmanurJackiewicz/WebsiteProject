858a339a4744a7e57e48319bf809928e4f3d0ddd
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-29 03:34:18 +0300

    DataAPI DTO ModelConfigurations created

---
+++ DataAPI/ModelConfigurations/SkillsConfigurations.cs
@@ -0,0 +1,16 @@
+﻿using DataAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAPI.ModelConfigurations
+{
+	public class SkillsConfigurations : IEntityTypeConfiguration<Skills>
+	{
+		public void Configure(EntityTypeBuilder<Skills> builder)
+		{
+			builder.Property(e => e.Description)
+			.HasColumnType("text")
+			.IsRequired(true);
+		}
+	}
+}

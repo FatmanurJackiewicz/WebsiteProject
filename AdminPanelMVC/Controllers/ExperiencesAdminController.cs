c9cad6db2a85894ee7bb2eb1e5676f88c5f52328
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-30 00:11:04 +0300

    AdminPanelMVC ExperiencesAdminController Created

---
+++ AdminPanelMVC/Controllers/ExperiencesAdminController.cs
@@ -0,0 +1,170 @@
+﻿using AdminPanelMVC.Models.Experiences;
+using DataAPI.DTOs.Experiences;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanelMVC.Controllers;
+
+[Authorize]
+public class ExperiencesAdminController : Controller
+{
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public ExperiencesAdminController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [Route("/experiences")]
+    [HttpGet]
+    public async Task<IActionResult> ExperiencesDetails()
+    {
+        var dataClient = _httpClientFactory.CreateClient("ApiClientData");
+        var response = await dataClient.GetAsync($"api/experiences");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return RedirectToAction(nameof(ExperiencesNoPage));
+        }
+
+        var experiencesDto = await response.Content.ReadFromJsonAsync<DetailsExperienceDto>();
+
+        var experiencesViewModel = new ExperiencesViewModel
+        {
+            Id = experiencesDto.Id,
+            Title = experiencesDto.Title,
+            Company = experiencesDto.Company,
+            StartDate = experiencesDto.StartDate,
+            EndDate = experiencesDto.EndDate,
+            Description = experiencesDto.Description
+        };
+
+        return View(experiencesViewModel);
+    }
+
+
+    [Route("/experiences-noPage")]
+    [HttpGet]
+    public async Task<IActionResult> ExperiencesNoPage()
+    {
+        return View();
+    }
+
+    [Route("/create-experiences")]
+    [HttpGet]
+    public IActionResult CreateExperiences()
+    {
+        return View();
+    }
+
+    [Route("/create-experiences")]
+    [HttpPost]
+    public async Task<IActionResult> CreateExperiences([FromForm] CreateExperiencesViewModel createExperiencesViewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(createExperiencesViewModel);
+
+        var createExperiencesDto = new CreateExperienceDto
+        {
+            Title = createExperiencesViewModel.Title,
+            Company = createExperiencesViewModel.Company,
+            StartDate = createExperiencesViewModel.StartDate,
+            EndDate = createExperiencesViewModel.EndDate,
+            Description = createExperiencesViewModel.Description
+        };
+
+        var client = _httpClientFactory.CreateClient("ApiClientData");
+        var response = await client.PostAsJsonAsync("api/experiences/createExperiences", createExperiencesDto);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorMessage = await response.Content.ReadAsStringAsync();
+            ViewBag.ErrorMessage = errorMessage;
+            return View(createExperiencesDto);
+        }
+
+        return RedirectToAction(nameof(ExperiencesDetails));
+    }
+
+
+    [Route("/update-experiences/{id}")]
+    [HttpGet]
+    public async Task<IActionResult> UpdateExperiences(int id)
+    {
+        var dataClient = _httpClientFactory.CreateClient("ApiClientData");
+        var response = await dataClient.GetAsync($"api/experiences/{id}");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return RedirectToAction(nameof(ExperiencesNoPage));
+        }
+
+        var experienceDto = await response.Content.ReadFromJsonAsync<UpdateExperienceDto>();
+
+        var updateExperiencesViewModel = new UpdateExperiencesViewModel
+        {
+            Id = experienceDto.Id,
+            Title = experienceDto.Title,
+            Company = experienceDto.Company,
+            StartDate = experienceDto.StartDate,
+            EndDate = experienceDto.EndDate,
+            Description = experienceDto.Description
+        };
+
+        return View(updateExperiencesViewModel);
+    }
+
+
+
+    [Route("/update-experiences")]
+    [HttpPost]
+    public async Task<IActionResult> UpdateExperiences([FromForm] UpdateExperiencesViewModel updateExperiencesViewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(updateExperiencesViewModel);
+
+        var updateExperienceDto = new UpdateExperienceDto
+        {
+            Id = updateExperiencesViewModel.Id,
+            Title = updateExperiencesViewModel.Title,
+            Company = updateExperiencesViewModel.Company,
+            StartDate = updateExperiencesViewModel.StartDate,
+            EndDate = updateExperiencesViewModel.EndDate,
+            Description = updateExperiencesViewModel.Description
+        };
+
+        var client = _httpClientFactory.CreateClient("ApiClientData");
+        var response = await client.PutAsJsonAsync($"api/experiences/{updateExperiencesViewModel.Id}", updateExperienceDto);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorMessage = await response.Content.ReadAsStringAsync();
+            ViewBag.ErrorMessage = errorMessage;
+            return View(updateExperiencesViewModel);
+        }
+
+        return RedirectToAction(nameof(ExperiencesDetails));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- AdminPanelMVC/Controllers/SkillsAdminController.cs
+++ AdminPanelMVC/Controllers/SkillsAdminController.cs
@@ -1,119 +1,129 @@
-﻿using AdminPanelMVC.Models.PersonalInfo;
-using AdminPanelMVC.Models.Skills;
-using DataAPI.DTOs.PersonalInfo;
+﻿using AdminPanelMVC.Models.Skills;
 using DataAPI.DTOs.Skills;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
-namespace AdminPanelMVC.Controllers;
-
-[Authorize]
-public class SkillsAdminController : Controller
+namespace AdminPanelMVC.Controllers
 {
-	private readonly IHttpClientFactory _httpClientFactory;
+    [Authorize]
+    public class SkillsAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
 
-	public SkillsAdminController(IHttpClientFactory httpClientFactory)
-	{
-		_httpClientFactory = httpClientFactory;
-	}
+        public SkillsAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
 
-	[Route("/skills")]
-	[HttpGet]
-	public async Task<IActionResult> SkillsDetails()
-	{
-		var dataClient = _httpClientFactory.CreateClient("ApiClientData");
-		var response = await dataClient.GetAsync($"api/skills");
-		if (!response.IsSuccessStatusCode)
-		{
-			return RedirectToAction(nameof(SkillsNoPage));
-		}
+        [Route("/skills")]
+        [HttpGet]
+        public async Task<IActionResult> SkillsDetails()
+        {
+            var dataClient = _httpClientFactory.CreateClient("ApiClientData");
+            var response = await dataClient.GetAsync("api/skills");
+            if (!response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(SkillsNoPage));
+            }
 
-		var skillsDto = await response.Content.ReadFromJsonAsync<SkillsDetailsDto>();
+            var skillsDto = await response.Content.ReadFromJsonAsync<SkillsDetailsDto>();
 
-		var skillsViewModel = new SkillsViewModel
-		{
-			Description = skillsDto.Description,
-		};
+            var skillsViewModel = new SkillsViewModel
+            {
+                Description = skillsDto.Description,
+                SkillsList = skillsDto.SkillsList // Eklenmesi gereken alan
+            };
 
-		return View(skillsViewModel);
-	}
+            return View(skillsViewModel);
+        }
 
+        public async Task<IActionResult> SkillsNoPage()
+        {
+            return View();
+        }
 
-	public async Task<IActionResult> SkillsNoPage()
-	{
-		return View();
-	}
+        [Route("/create-skills")]
+        [HttpGet]
+        public IActionResult CreateSkills()
+        {
+            return View();
+        }
 
-	[Route("/create-skills")]
-	[HttpGet]
-	public IActionResult CreateSkills()
-	{
-		return View();
-	}
+        [Route("/create-skills")]
+        [HttpPost]
+        public async Task<IActionResult> CreateSkills([FromForm] CreateSkillsViewModel createSkillsViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(createSkillsViewModel);
 
-	[Route("/create-skills")]
-	[HttpPost]
-	public async Task<IActionResult> CreateSkills([FromForm] CreateSkillsViewModel createSkillsViewModel)
-	{
-		if (!ModelState.IsValid)
-			return View(createSkillsViewModel);
+            var createSkillsDto = new CreateSkillsDto
+            {
+                Description = createSkillsViewModel.Description,
+                SkillsList = createSkillsViewModel.SkillsList
+            };
 
-		// ViewModel'den DTO'ya dönüştürme işlemi yapılırken Skills listesini de ekliyoruz
-		var createSkillsDto = new CreateSkillsDto
-		{
-			Description = createSkillsViewModel.Description,
-			SkillsList = createSkillsViewModel.SkillsList // Skills listesini ekliyoruz
-		};
+            var client = _httpClientFactory.CreateClient("ApiClientData");
+            var response = await client.PostAsJsonAsync("api/skills/createSkills", createSkillsDto);
 
-		var client = _httpClientFactory.CreateClient("ApiClientData");
-		var response = await client.PostAsJsonAsync("api/skills/createSkills", createSkillsDto);
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                ViewBag.ErrorMessage = errorMessage;
+                return View(createSkillsViewModel);
+            }
 
-		if (!response.IsSuccessStatusCode)
-		{
-			var errorMessage = await response.Content.ReadAsStringAsync();
-			ViewBag.ErrorMessage = errorMessage;
-			return View(createSkillsDto);
-		}
+            return RedirectToAction(nameof(SkillsDetails));
+        }
 
-		return RedirectToAction(nameof(SkillsDetails));
-	}
+        [Route("/update-skills")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateSkills(int id) // ID ile formu almak için
+        {
+            var dataClient = _httpClientFactory.CreateClient("ApiClientData");
+            var response = await dataClient.GetAsync($"api/skills/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(SkillsNoPage));
+            }
 
+            var skillsDto = await response.Content.ReadFromJsonAsync<UpdateSkillsDto>();
 
+            var updateSkillsViewModel = new UpdateSkillsViewModel
+            {
+                Description = skillsDto.Description,
+                SkillsList = skillsDto.SkillsList
+            };
 
-	[Route("/update-skills")]
-	[HttpGet]
-	public IActionResult UpdatePersonalInfo()
-	{
-		return View();
-	}
+            return View(updateSkillsViewModel);
+        }
 
+        [Route("/update-skills")]
+        [HttpPost]
+        public async Task<IActionResult> UpdateSkills([FromForm] UpdateSkillsViewModel updateSkillsViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(updateSkillsViewModel);
 
-	[Route("/update-skills")]
-	[HttpPost]
-	public async Task<IActionResult> UpdateSkills([FromForm] UpdateSkillsViewModel updateSkillsViewModel)
-	{
-		if (!ModelState.IsValid)
-			return View(updateSkillsViewModel);
+            var updateSkillsDto = new UpdateSkillsDto
+            {
+                Description = updateSkillsViewModel.Description,
+                SkillsList = updateSkillsViewModel.SkillsList
+            };
 
-		// ViewModel'den DTO'ya dönüştürme işlemi yapılırken Skills listesini de ekliyoruz
-		var updateSkillsDto = new UpdateSkillsDto
-		{ // Güncellemek istediğiniz kaydın Id'si
-			Description = updateSkillsViewModel.Description,
-			SkillsList = updateSkillsViewModel.SkillsList // Skills listesini ekliyoruz
-		};
+            var client = _httpClientFactory.CreateClient("ApiClientData");
+            var response = await client.PutAsJsonAsync("api/skills/updateSkills", updateSkillsDto);
 
-		var client = _httpClientFactory.CreateClient("ApiClientData");
-		var response = await client.PutAsJsonAsync($"api/skills/updateSkills", updateSkillsDto);
+            if (!response.IsSuccessStatusCode)
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                ViewBag.ErrorMessage = errorMessage;
+                return View(updateSkillsViewModel);
+            }
 
-		if (!response.IsSuccessStatusCode)
-		{
-			var errorMessage = await response.Content.ReadAsStringAsync();
-			ViewBag.ErrorMessage = errorMessage;
-			return View(updateSkillsViewModel);
-		}
-
-		return RedirectToAction(nameof(SkillsDetails));
-	}
-
-
+            return RedirectToAction(nameof(SkillsDetails));
+        }
+    }
 }

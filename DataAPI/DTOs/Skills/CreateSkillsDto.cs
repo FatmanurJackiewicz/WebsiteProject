14768857858a28e568c6d9fb96660283b6af0b8d
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-29 03:33:16 +0300

    DataAPI DTO Skills created

---
+++ DataAPI/DTOs/Skills/CreateSkillsDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAPI.DTOs.Skills
+{
+	public class CreateSkillsDto
+	{
+		[Required]
+		public string Description { get; set; }
+	}
+}
---
+++ DataAPI/DTOs/Skills/SkillsDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAPI.DTOs.Skills
+{
+	public class SkillsDetailsDto
+	{
+		[Required]
+		public string Description { get; set; }
+	}
+}

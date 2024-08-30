cb7d6062665f955df18c8429c7167be2ab16a6e1
Fatmanur Jackiewicz <faseselv@gmail.com>
2024-08-31 01:07:45 +0300

    PortfoyMVC updated

---
+++ PortfoyMVC/Models/SkillsPortfoyViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfoyMVC.Models;
+
+public class SkillsPortfoyViewModel
+{
+    [Required]
+    public List<string> SkillsList { get; set; }
+}
---
+++ PortfoyMVC/ViewComponents/SkillsViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using DataAPI.DTOs.Skills;
+using Microsoft.AspNetCore.Mvc;
+using PortfoyMVC.Models;
+
+namespace PortfoyMVC.ViewComponents;
+
+public class SkillsViewComponent : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public SkillsViewComponent(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+
+        var dataClient = _httpClientFactory.CreateClient("ApiClientData");
+        var response = await dataClient.GetAsync($"api/skills");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorResponse = await response.Content.ReadAsStringAsync();
+            ModelState.AddModelError(string.Empty, $"Bir hata oluştu: {errorResponse}");
+            return View();
+        }
+
+        var skillsDetailsDto = await response.Content.ReadFromJsonAsync<SkillsDetailsDto>();
+
+        var skillsPortfoyViewModel = new SkillsPortfoyViewModel
+        {
+            SkillsList = skillsDetailsDto.SkillsList
+        };
+
+        return View(skillsPortfoyViewModel);
+    }
+}
